8841823d7b50d49eb6ce0ae299325eece993873e
rcantuario <r.cantuario@gmail.com>
2018-06-28 23:25:59 -0300

    starting checkout page

--- src/components/Checkout/index.js
+++ src/components/Checkout/index.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Checkout extends Component {
   render() {
-    return <h1> Hello Checkout </h1>;
+    return <h1> Hello Checkout {this.props.items.length}</h1>;
   }
 }
 
-export default Checkout;
+//export default Checkout;
+
+const mapStateToProps = state => ({
+  items: state.productReducer.products
+});
+
+export default connect(mapStateToProps)(Checkout);
